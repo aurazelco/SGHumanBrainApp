d87379e870789983d2da5890f90e49f4d6cd266f
Aura Zelco <aura.zelco@gmail.com>
2023-05-23 09:46:53 +0200

    Fixes link in Source tab

--- custom_scripts/Source.R
+++ custom_scripts/Source.R
@@ -7,7 +7,7 @@ SourceUI <- function(id, label = "source_code"){
     fluidRow(
       br(),
       p("The open source code for this application is available on Github :"),
-      tags$a(href = "https://github.com/aurazelco/SGHumanBrain.git", "https://github.com/aurazelco/SGHumanBrain.git")
+      tags$a(href = "https://github.com/aurazelco/SGHumanBrainApp.git", "https://github.com/aurazelco/SGHumanBrainApp.git")
       
     ),
     
